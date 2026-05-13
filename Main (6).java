e1a11d70d99f26f70c35a4623b83fbc56269317e
olesa1212 <o6635970@gmail.com>
2026-05-13 17:28:07 +0300

    Add files via upload

---
+++ Main (6).java
@@ -0,0 +1,76 @@
+import javax.xml.transform.SourceLocator;
+import java.sql.SQLOutput;
+
+void main() {
+    Scanner in = new Scanner(System.in);
+    Scanner on = new Scanner(System.in);
+    User[] arr = new People[4];
+    for(int i = 0; i <= 1; i++) {
+        System.out.println("Введите ФИО:");
+        String fio = in.nextLine();
+        System.out.println("Введите возраст:");
+        int age = on.nextInt();
+        System.out.println("Введите должность:");
+        String post = in.nextLine();
+        arr[i] = new Teacher(fio, age, post);
+        System.out.println("Введите логин:");
+        String login = in.nextLine();
+        System.out.println("Введите пароль:");
+        String password = in.nextLine();
+        arr[i].user(login, password);
+    }
+    for(int i = 2; i <= 3; i++) {
+        System.out.println("Введите ФИО:");
+        String fio = in.nextLine();
+        System.out.println("Введите возраст:");
+        int age = on.nextInt();
+        System.out.println("Введите должность:");
+        String post = in.nextLine();
+        arr[i] = new Student(fio, age, post);
+        System.out.println("Введите логин:");
+        String login = in.nextLine();
+        System.out.println("Введите пароль:");
+        String password = in.nextLine();
+        arr[i].user(login, password);
+    }
+}
+
+interface User {
+    void user(String login, String password);
+ }
+
+ abstract class People implements User {
+    String fio;
+    int age;
+    String post;
+
+    People(String fio, int age, String post) {
+        this.fio = fio;
+        this.age = age;
+        this.post = post;
+    }
+ }
+
+ class Teacher extends People {
+     Teacher(String fio, int age, String post) {
+         super(fio, age, post);
+     }
+
+     @Override
+     public void user(String login, String password) {
+         System.out.println("Login:" + login);
+         System.out.println("Password:" + password);
+     }
+ }
+
+ class Student extends People {
+     Student(String fio, int age, String post) {
+         super(fio, age, post);
+     }
+
+     @Override
+     public void user(String login, String password) {
+         System.out.println("Login:" + login);
+         System.out.println("Password:" + password);
+     }
+ }
